0bad5a316c375c9f469c9305ac5ded3bc068c4d8
Taras Gasyuk <tarasgasyuk@gmail.com>
2023-04-02 15:03:59 +0300

    Add absolute path

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: [
     '../src/components/**/*.stories.mdx',
@@ -21,5 +23,20 @@ module.exports = {
   },
   typescript: {
     reactDocgen: 'react-docgen-typescript-plugin',
+  },
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.modules = [path.resolve(__dirname, './src'), 'node_modules'];
+    config.resolve.alias['@components'] = path.join(__dirname, '../src/components');
+    config.resolve.alias['@utils'] = path.join(__dirname, '../src/utils');
+    config.resolve.alias['@store'] = path.join(__dirname, '../src/store');
+    config.resolve.alias['@modules'] = path.join(__dirname, '../src/modules');
+    config.resolve.alias['@api'] = path.join(__dirname, '../src/modules');
+    config.resolve.alias['@lib'] = path.join(__dirname, '../src/lib');
+    config.resolve.alias['@types'] = path.join(__dirname, '../src/types');
+    config.resolve.alias['@hooks'] = path.join(__dirname, '../src/hooks');
+    config.resolve.alias['@data'] = path.join(__dirname, '../src/data');
+    config.resolve.alias['@config'] = path.join(__dirname, '../src/config');
+
+    return config;
   },
 };
